9a0c65eedbf585012c2f5fbfba77673e7e64aba6
matteo <1434386636@qq.com>
2020-08-22 20:10:14 +0800

    add pertag viewontag rotatestack and clean code

--- config.h
+++ config.h
@@ -1,28 +1,27 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int gappx     = 15;        /* gaps between windows */
-static const unsigned int snap      = 32;       /* snap pixel */
-static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "SauceCodePro Nerd Font Mono:size=10" };
-//static const char dmenufont[]       = "SauceCodePro Nerd Font Mono:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#ffffff";
-//static const char col_cyan[]        = "#37474F";
-static const char col_cyan[]        = "#333333";
-static const unsigned int baralpha = 0xd0;
+static const unsigned int borderpx 	  = 1;        /* border pixel of windows */
+static const unsigned int gappx    	  = 10;        /* gaps between windows */
+static const unsigned int snap     	  = 32;       /* snap pixel */
+static const int showbar           	  = 1;        /* 0 means no bar */
+static const int topbar            	  = 1;        /* 0 means bottom bar */
+static const char *fonts[]         	  = { "SauceCodePro Nerd Font Mono:size=10" };
+static const char col_gray1[]      	  = "#222222";
+static const char col_gray2[]      	  = "#444444";
+static const char col_gray3[]      	  = "#bbbbbb";
+static const char col_gray4[]      	  = "#ffffff";
+static const char col_cyan[]       	  = "#333333";
+static const unsigned int baralpha 	  = 0xd0;
 static const unsigned int borderalpha = OPAQUE;
-static const char *colors[][3]      = {
+static const Bool viewontag           = True;     /* Switch view on tag switch */
+static const char *colors[][3]        = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
 	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
 	[SchemeHid]  = { col_cyan,  col_gray1, col_cyan  },
 };
-static const unsigned int alphas[][3]      = {
+static const unsigned int alphas[][3] = {
 	/*               fg      bg        border     */
 	[SchemeNorm] = { OPAQUE, baralpha, borderalpha },
 	[SchemeSel]  = { OPAQUE, baralpha, borderalpha },
@@ -65,23 +64,20 @@ static const Layout layouts[] = {
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
-//static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-//static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]   = { "st", NULL };
 static const char *chromium[]  = { "chromium", "--proxy-server=socks5://127.0.0.1:1089", NULL };
-static const char *ranger[]  = { "st", "-e", "ranger", NULL };
-static const char *nvim[]  = { "st", "-e", "nvim", NULL };
-static const char *music[]  = { "electron-netease-cloud-music", NULL };
-static const char *flameshot[]  = { "flameshot", "gui", NULL };
-static const char *upvol[]  = { "/home/matteo/scripts/vol-up.sh", NULL };
-static const char *downvol[]  = { "/home/matteo/scripts/vol-down.sh", NULL };
-static const char *mutevol[]  = { "/home/matteo/scripts/vol-toggle.sh", NULL };
-static const char *rofirun[]  = { "rofi", "-show", "run", NULL };
-static const char *rofiapp[]  = { "rofi", "-show", "drun", NULL };
+static const char *ranger[]    = { "st", "-e", "ranger", NULL };
+static const char *nvim[]      = { "st", "-e", "nvim", NULL };
+static const char *music[]     = { "electron-netease-cloud-music", NULL };
+static const char *flameshot[] = { "flameshot", "gui", NULL };
+static const char *upvol[]     = { "/home/matteo/scripts/vol-up.sh", NULL };
+static const char *downvol[]   = { "/home/matteo/scripts/vol-down.sh", NULL };
+static const char *mutevol[]   = { "/home/matteo/scripts/vol-toggle.sh", NULL };
+static const char *rofirun[]   = { "rofi", "-show", "run", NULL };
+static const char *rofiapp[]   = { "rofi", "-show", "drun", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	//{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_a,      spawn,          {.v = rofiapp} },
 	{ MODKEY,                       XK_d,      spawn,          {.v = rofirun} },
 	{ MODKEY,           			XK_Return, spawn,          {.v = termcmd } },
@@ -94,6 +90,8 @@ static Key keys[] = {
 	{ MODKEY,						XK_F11,	   spawn,          {.v = downvol } },
 	{ MODKEY,						XK_F9,	   spawn,          {.v = mutevol } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
+	{ MODKEY|ShiftMask,             XK_j,      rotatestack,    {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_k,      rotatestack,    {.i = -1 } },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_h,      incnmaster,     {.i = +1 } },
