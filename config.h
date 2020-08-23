71d5ab8b9cf31f25c70ba0ab7e0a9172b83490f6
matteo <1434386636@qq.com>
2020-08-23 11:39:12 +0800

    update

--- config.h
+++ config.h
@@ -28,7 +28,7 @@ static const unsigned int alphas[][3] = {
 };
 
 /* tagging */
-static const char *tags[] = { "", "", "", "", "", "", "", "", "" };
+static const char *tags[] = { "", "", "", "", "", "", "", "", "" };
 
 static const Rule rules[] = {
 	/* xprop(1):
